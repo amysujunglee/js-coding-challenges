f54a9c8dda64bb99727eccb4a2b30f63488e28cf
amysujunglee <amy.sujunglee@gmail.com>
2020-11-15 09:55:13 -0800

    adding timeConversion algorithm

---
+++ timeConversion.js
@@ -0,0 +1,14 @@
+let time = "12:45:54PM";
+
+let ampm = time.slice(-2);
+let timeArr = time.slice(0, -2).split(":");
+
+if (ampm === "AM" && timeArr[0] === "12") {
+  timeArr[0] = "00";
+} else if (ampm === "PM" && timeArr[0] === "12") {
+  timeArr[0] = "12";
+} else if (ampm === "PM" && timeArr[0] !== "12") {
+  timeArr[0] = parseInt(timeArr[0]) + 12;
+}
+
+console.log(timeArr.join(":"));
